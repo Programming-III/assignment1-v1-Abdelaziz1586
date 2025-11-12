c157d502fabc1b875058ebbf9fbe654f3b4894cb
Abdelaziz Mohamed <81268592+Abdelaziz1586@users.noreply.github.com>
2025-11-12 15:00:22 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,36 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
-
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal {
+    string name;
+    int age;
+    bool isHungry;
+public:
+    Animal();
+    Animal(string name, int age, bool isHungry);
 
+    virtual void display() {
+        cout << name << "(Age: " << age << ", " << (isHungry ? "Hungry" : "Not Hungry") << ")" << endl;
+    }
 
+    void feed();
+
+    virtual ~Animal();
+
+    string getName() {
+        return name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    bool hungry() {
+        return isHungry;
+    }
+
+};
 
 #endif
